dc9c2d67ace2e410d41f4916e554bbf713c0fe6d
ajmal-j <ajmaljaleel7860@gmail.com>
2024-01-25 23:54:46 +0530

    added c , cpp , python : java need fix

---
+++ backend/code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, c!\n");
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,44 @@
 import express from "express";
-import { node } from "compile-run";
+import { node, cpp, c, java, python } from "compile-run";
 import fs from "fs";
 import path from "path";
 
 const app = express();
 
 app.get("/", async (req, res) => {
+  const language = req.query.l;
+  if (!language) return res.end("select a language");
   try {
+    let selectedExtension =
+      language.toLowerCase() === "node"
+        ? ".js"
+        : language.toLowerCase() === "cpp"
+        ? ".cpp"
+        : language.toLowerCase() === "c"
+        ? ".c"
+        : language.toLowerCase() === "java"
+        ? ".java"
+        : ".py";
+
     let __dirname = path.resolve(path.dirname(""));
-    const filePath = path.join(__dirname, "code.js");
+    const filePath = path.join(__dirname, `code${selectedExtension}`);
     const fileContent = fs.readFileSync(filePath, "utf-8");
-    let result = await node.runSource(fileContent);
+    let selectedLanguage =
+      language.toLowerCase() === "node"
+        ? node
+        : language.toLowerCase() === "cpp"
+        ? cpp
+        : language.toLowerCase() === "c"
+        ? c
+        : language.toLowerCase() === "java"
+        ? java
+        : python;
+    let result = await selectedLanguage.runSource(fileContent);
     if (result.stderr) {
       const errorMessage = result.stderr.split("\n").slice(1).join(",");
       res.send(errorMessage);
     } else {
+      console.log(result);
       res.send(result.stdout);
     }
   } catch (err) {
